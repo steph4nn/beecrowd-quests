78f24bf46131c4985346ea47eb0bd6a501389d0b
Lauro Stephan <110504296+steph4nn@users.noreply.github.com>
2023-10-27 18:50:45 +0000

    ex2290 runtime error 373kb

--- python/ex2290.py
+++ python/ex2290.py
@@ -1,14 +1,13 @@
 while True:
-    saida = []
+    saida = ''
     n = int(input())
     if n == 0:
         break
     vetor = list(map(int, input().split()))
     vetorConjunto = set(vetor)
-    listaaux = list(vetor)
-    for i in range(len(listaaux)):
-        count = vetor.count(listaaux[i])
+    listaaux = list(vetorConjunto)
+    for index, element in enumerate(listaaux):
+        count = vetor.count(listaaux[index])
         if count %2 ==1:
-            saida.append(listaaux[i])
-    strings = [str(numero) for numero in saida]
-    print(' '.join(sorted(strings)))
+            saida+=f'{listaaux[index]} '
+    print(saida)
